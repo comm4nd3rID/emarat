83f2fa4be150f49f5676d77476250cff9798dcc3
Sellect * From Comm4nd3r <68114044+comm4nd3rID@users.noreply.github.com>
2025-02-04 01:23:41 +0330

    use cors package

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 const app = express();
 const port = 3000;
+
+app.use(cors({
+    origin: "http://127.0.0.1:5500"
+}));
 
 app.get("/", (req,res) => {
     res.send({
